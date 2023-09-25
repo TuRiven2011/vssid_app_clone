3c26662c9152ad8b4afe5360ebbb4b623d68345d
TuRiven2011 <phambatu111@gmail.com>
2023-09-26 01:00:54 +0700

    commit

---
+++ vssid_app_clone/Extension/UIViewControllerExtension.swift
@@ -0,0 +1,36 @@
+//
+//  UIViewControllerExtension.swift
+//  vssid_app_clone
+//
+//  Created by Phạm Bá Tú on 25/09/2023.
+//
+
+import Foundation
+import UIKit
+
+protocol StoryBoardIdentifiable: AnyObject {
+    static var storyboard: UIStoryboard { get }
+}
+
+extension StoryBoardIdentifiable {
+    static var storyboard: UIStoryboard {
+        return UIStoryboard(name: String(describing: self), bundle: Bundle(for: self))
+    }
+}
+
+extension StoryBoardIdentifiable where Self: UIViewController {
+    
+    static func instantiate(creator: @escaping (NSCoder) -> Self?) -> Self {
+        if #available(iOS 13.0, *) {
+            return storyboard.instantiateViewController(identifier: storyboardIdentifier, creator: creator)
+        } else {
+            return storyboard.instantiateViewController(withIdentifier: storyboardIdentifier) as! Self
+        }
+    }
+}
+
+extension UIViewController: StoryBoardIdentifiable {
+    static var storyboardIdentifier: String {
+        return String(describing: self)
+    }
+}
---
+++ vssid_app_clone/ViewController/SideMenu/SideMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SideMenuViewController.swift
+//  vssid_app_clone
+//
+//  Created by Phạm Bá Tú on 25/09/2023.
+//
+
+import UIKit
+
+class SideMenuViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
