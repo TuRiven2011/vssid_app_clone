b1679b742ed6fad2969357aac7ae746b9f591e14
TuRiven2011 <phambatu111@gmail.com>
2023-09-27 02:12:36 +0700

    commit

---
+++ vssid_app_clone/Common/CommonUtils.swift
@@ -0,0 +1,39 @@
+//
+//  CommonUtils.swift
+//  vssid_app_clone
+//
+//  Created by Phạm Bá Tú on 27/09/2023.
+//
+
+import Foundation
+import UIKit
+
+class CommonUtils {
+    static let shared: CommonUtils = CommonUtils()
+    
+    private init() {
+        
+    }
+    
+    func openURL(urlString: String) {
+        guard let url = URL(string: urlString) else {
+          return //be safe
+        }
+
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    
+    func makePhoneCall(phoneNumber: String) {
+        if let url = URL(string: "tel://\(phoneNumber)"), UIApplication.shared.canOpenURL(url) {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+}
