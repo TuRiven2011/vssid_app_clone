cf4ea306a591b022ec1ed6286eeb546ff894b874
An Hung <dangahung111@gmail.com>
2023-10-02 19:43:16 +0700

    .

---
+++ vssid_app_clone/ViewController/Insert Infor/InsertInforViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InsertInforViewController.swift
+//  vssid_app_clone
+//
+//  Created by Đặng An Hưng on 02/10/2023.
+//
+
+import UIKit
+
+class InsertInforViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
