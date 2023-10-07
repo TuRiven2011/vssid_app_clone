f37dc67534d228b3c9789ce81814a61f81187f12
TuRiven2011 <phambatu111@gmail.com>
2023-10-07 12:10:39 +0700

    commit

--- vssid_app_clone/ViewController/Infor/InsuranceInfor/InsuranceExample/InsuranceExampleViewController.swift
+++ vssid_app_clone/ViewController/Infor/InsuranceInfor/InsuranceExample/InsuranceExampleViewController.swift
@@ -2,17 +2,47 @@
 
 import UIKit
 
-class InsuranceExampleViewController: UIViewController {
+class InsuranceExampleViewController: UIViewController, UINavigationControllerDelegate {
     @IBOutlet weak var dismissBtnView: UIImageView!
+    
+    @IBOutlet weak var imgBHXH: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         dismissBtnView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapDismiss(_:))))
+        
+        //        viewBHXH.transform = CGAffineTransform(rotationAngle: -(.pi/2))
+        
+        imgBHXH.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleAddImage)))
     }
-
+    
     @objc func didTapDismiss(_ gesture: UITapGestureRecognizer) {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    @objc func handleAddImage() {
+        imagePicker.modalPresentationStyle = UIModalPresentationStyle.currentContext
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true)
+    }
 
+}
+
+extension InsuranceExampleViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let tempImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+
+        imgBHXH.image = tempImage
+        
+        imagePicker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePicker.dismiss(animated: true)
+    }
 }
